b8e182f0d49fca62f12139ea870d2b6f43322568
luhuaqin <1684277580@qq.com>
2022-08-05 16:39:47 +0800

    安装echarts依赖

--- src/views/main/analysis/dashboard/dashboard.vue
+++ src/views/main/analysis/dashboard/dashboard.vue
@@ -1,18 +1,62 @@
 <template>
   <div class="dashboard">
-    <h2>dashboard</h2>
+    <div id="myChart" ref="divRef"></div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref, onMounted } from 'vue'
+
+import * as echarts from 'echarts'
 
 export default defineComponent({
   name: 'dashboard',
   setup() {
-    return {}
+    const divRef = ref<HTMLElement>()
+    onMounted(() => {
+      const echartInstance = echarts.init(divRef.value!, 'dark', {
+        renderer: 'svg'
+      }) // 断言确定divRef内有元素
+      // 指定图表的配置项和数据
+      var option = {
+        title: {
+          text: 'ECharts 入门示例'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'shadow'
+          }
+        },
+        legend: {
+          data: ['销量']
+        },
+        xAxis: {
+          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+        },
+        yAxis: {},
+        series: [
+          {
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+          }
+        ]
+      }
+      // 使用刚指定的配置项和数据显示图表。
+      echartInstance.setOption(option)
+    })
+
+    return {
+      divRef
+    }
   }
 })
 </script>
 
-<style scoped></style>
+<style scoped lang="less">
+#myChart {
+  width: 450px;
+  height: 300px;
+}
+</style>
